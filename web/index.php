96f67bc20a63406607301a61eed229c960123608
Ben Sandberg <ben@pdxfixit.com>
2015-03-12 13:17:34 -0700

    remove web folder

--- web/index.php
+++
@@ -1,4 +0,0 @@
-<h1>PDXfixIT Development Virtual Machine Default Web Site</h1>
-<p>If you've reached this screen, we couldn't find the web files for the requested host. Please copy the <code>config.yaml.template</code> file to <code>config.yaml</code>, and change the values within, so that it points to valid folders on your system.</p>
-<hr>
-<?php phpinfo(); ?>
